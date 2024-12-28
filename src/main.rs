60e686413dd7f6f77f79b69eceb11604c7f23c36
thomasjsk <thomas.jasiuk@gmail.com>
2024-12-28 18:29:41 +0100

    Switch to one-pad

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use hex;
 use std::borrow::Cow;
 
-fn extrapolate_key(base_key: &u16, target_len: usize) -> Vec<u8> {
-    let key_b = base_key.to_be_bytes();
+fn extrapolate_key(base_key: &str, target_len: usize) -> Vec<u8> {
+    let key_b = base_key.as_bytes();
     let multiplier = target_len / key_b.len();
 
     let mut key_ext: Vec<u8> = Vec::with_capacity(key_b.len());
@@ -17,9 +17,9 @@ fn extrapolate_key(base_key: &u16, target_len: usize) -> Vec<u8> {
     key_ext
 }
 
-fn cipher(text: &str, key_hex: &u16) -> Vec<u8> {
+fn cipher(text: &str, key: &str) -> Vec<u8> {
     let text_b = Vec::from(text.as_bytes());
-    let key_b = extrapolate_key(&key_hex, text.len());
+    let key_b = extrapolate_key(key, text.len());
 
     let mut cipher: Vec<u8> = Vec::with_capacity(text_b.len());
     for i in 0..text_b.len() {
@@ -29,8 +29,8 @@ fn cipher(text: &str, key_hex: &u16) -> Vec<u8> {
     cipher
 }
 
-fn vigenere(plaintext: &str, key: u16) -> String {
-    let cipher_bytes = cipher(plaintext, &key);
+fn one_pad(plaintext: &str, key: &str) -> String {
+    let cipher_bytes = cipher(plaintext, key);
     let ciphertext = match String::from_utf8_lossy(&cipher_bytes) {
         Cow::Owned(v) => v,
         Cow::Borrowed(v) => v.to_string(),
@@ -41,12 +41,13 @@ fn vigenere(plaintext: &str, key: u16) -> String {
 }
 
 fn main() {
-    let plaintext = "Hello!";
-    let enc_key: u16 = 0xA12F;
-    let encrypted = vigenere(plaintext, enc_key);
+    let plaintext = "Hello World!";
+    let enc_key = "Goodbye";
+    let encrypted = one_pad(plaintext, enc_key);
+    let decrypted = one_pad(&encrypted, enc_key);
     println!("---------------------------------");
     println!("plaintext: {}", plaintext);
-
-    println!("encrypted: {}", encrypted);
+    println!("encrypted: {:?}", encrypted);
+    println!("decrypted: {}", decrypted);
     println!("---------------------------------");
 }
